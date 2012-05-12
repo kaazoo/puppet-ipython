6f5e688976a16b9fae0d17269e523354815fb6ad
Andreas Schroeder <andreas@drqueue.org>
2012-05-13 01:40:05 +0200

    move software installation to install subclass

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,25 +1,5 @@
 class ipython::config {
   
-  package { ["python${ipython::python_version}", "python${ipython::python_version}-dev", "git-core"]:
-    ensure => present,
-  }
-
-  file { "/etc/apt/sources.list.d/chris-lea-zeromq-oneiric.list":
-    ensure => present,
-    content => "deb http://ppa.launchpad.net/chris-lea/zeromq/ubuntu oneiric main",
-    notify => Exec["aptitude update"],
-    alias => "ppa-zeromq",
-  }
-
-  exec { "aptitude update":
-    command => "/usr/bin/aptitude update",
-    refreshonly => true,
-  }
-
-  package { "libzmq-dev":
-    ensure => present,
-    require => File["ppa-zeromq"],
-  }
 
 
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,27 @@ class ipython::install {
 
   search Ipython::Functions
 
+  package { ["python${ipython::python_version}", "python${ipython::python_version}-dev", "git-core"]:
+    ensure => present,
+  }
+
+  file { "/etc/apt/sources.list.d/chris-lea-zeromq-oneiric.list":
+    ensure => present,
+    content => "deb http://ppa.launchpad.net/chris-lea/zeromq/ubuntu oneiric main",
+    notify => Exec["aptitude update"],
+    alias => "ppa-zeromq",
+  }
+
+  exec { "aptitude update":
+    command => "/usr/bin/aptitude update",
+    refreshonly => true,
+  }
+
+  package { "libzmq-dev":
+    ensure => present,
+    require => File["ppa-zeromq"],
+  }
+
   # clone from git repository
   exec { "git clone -b ${ipython::git_branch} git://github.com/ipython/ipython.git":
     cwd     => "/root",
